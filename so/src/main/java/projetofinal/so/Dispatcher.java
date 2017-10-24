e69b541e8e349d9d6dcaa4199c91f99bd2ee766a
tuliolima <tulioabner@gmail.com>
2017-10-24 16:33:42 -0200

    Definindo comportamento do Dispatcher

--- so/src/main/java/projetofinal/so/Dispatcher.java
+++ so/src/main/java/projetofinal/so/Dispatcher.java
@@ -4,6 +4,7 @@ import projetofinal.so.arquivos.GerenciaArquivo;
 import projetofinal.so.filas.GerenciaFila;
 import projetofinal.so.memoria.GerenciaMemoria;
 import projetofinal.so.processos.GerenciaProcesso;
+import projetofinal.so.processos.Processo;
 import projetofinal.so.recursos.GerenciaRecurso;
 
 public class Dispatcher{
@@ -13,14 +14,16 @@ public class Dispatcher{
 	private GerenciaMemoria gerenciadorMemoria;
 	private GerenciaProcesso gerenciadorProcesso;
 	private GerenciaArquivo gerenciadorArquivo;
-	private GerenciaFila gerenciadorFila;
+	private GerenciaFila escalonador;
 	private GerenciaRecurso gerenciadorRecurso;
+	
+	private int clock;
 	
 	private void iniciaGerencia() {
 		gerenciadorMemoria = new GerenciaMemoria();
 		gerenciadorProcesso = new GerenciaProcesso();
 		gerenciadorArquivo = new GerenciaArquivo();
-		gerenciadorFila = new GerenciaFila();
+		escalonador = new GerenciaFila();
 		gerenciadorRecurso = new GerenciaRecurso();
 		
 	}
@@ -35,5 +38,89 @@ public class Dispatcher{
 		}
 		return instancia;
 	}
+	
+	public void executarProcessos() {
+		Processo processo;
+		clock = 0;
+		
+		while(gerenciadorProcesso.temNovosProcessos() || !escalonador.vazio()) {
+			//Verifica se tem processos para serem criados
+			//ou processos no escalonador
+			
+			do { //Cria todos os processos de já surgiram
+				processo = gerenciadorProcesso.proximoProcesso(clock);
+				
+				if (processo != null) {
+					if (gerenciadorMemoria.reservarMemoria(processo)) {
+						escalonador.escalonarProcesso(processo);
+					} else {
+						gerenciadorProcesso.excluirProcesso(processo);
+					}
+				}
+			} while (processo != null);
+			
+			processo = escalonador.proximoProcesso();
+			if (processo != null) {
+				if (processo.getPrioridade() == 0) {
+					//Executa até acabar
+				} else {
+					//Executa até acabar o quantum
+				}
+			}
+		}
+		
+	}
+	
+/*
+	Dispatcher iniciado
+	
+	Ler arquivos txt {
+		Se leu um processo {
+			adiciona o processo na lista de processos
+		}
+	Leitura terminada
+	}
+	
+	Iniciar contador do relógio
+	
+	loop do relógio {
+		verifica na lista de processos se tem algum para ser criado
+		se tem {
+			pede para o gerenciador de memória reservar memória pro processo
+			se tem memória {
+				salva no processo o endereço da memória
+				salva na memória o nome do processo
+				//pensar sobre a melhor estratégia
+				manda para o escalonador
+			} se não tem memória {
+				recusa a criação do processo
+			}
+		}
+		
+		pede pro escalonador o processo a ser executado
+		se for de tempo real {
+			Executa todas as instruções
+		}
+		se for de usuário {
+			Executa até acabar o quantum
+		}
+		
+	}
+	Sai do loop quando não houverem processos a serem criados
+	ou processos no escalonador
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+
+
+
+ */
 	
 }
