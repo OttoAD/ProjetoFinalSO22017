bfe8620fb48d427074fd21eae293865c7d5385dc
OttoAD <otavioad@hotmail.com>
2017-11-05 17:06:19 -0200

    Alterações nos arquivos de processos.

--- so/src/main/java/projetofinal/so/filas/GerenciaFila.java
+++ so/src/main/java/projetofinal/so/filas/GerenciaFila.java
@@ -5,18 +5,21 @@ import projetofinal.so.processos.Processo;
 public class GerenciaFila implements Escalonador {
 
 	public boolean vazio() {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public boolean escalonarProcesso(Processo processo) {
-		// TODO Auto-generated method stub
 		return false;
 	}
 
 	public Processo proximoProcesso() {
-		// TODO Auto-generated method stub
 		return null;
+	}
+
+	public void excluirProcesso(Processo process) {
+	}
+
+	public void diminuirPrioridade(Processo process) {
 	}
 
 }
--- so/src/main/java/projetofinal/so/processos/GerenciaProcesso.java
+++ so/src/main/java/projetofinal/so/processos/GerenciaProcesso.java
@@ -3,19 +3,37 @@ import projetofinal.so.dados.processo.ListaProcesso;
 public class GerenciaProcesso implements BancoDeProcessos{
 	
 	private ListaProcesso processosNaoAlocados;
-	private ListaProcesso processosAlocados; //??????
+	private ListaProcesso processosAlocados;
 	
 	public GerenciaProcesso(ListaProcesso lista) {
 		this.processosNaoAlocados = lista;
+		this.processosAlocados = new ListaProcesso();
 	}
 	
 	public boolean temNovosProcessos(){
-		try {
-			this.lista.getProcesso(0);
-			return true;
-		}catch(ProcessoInexistenteException pie) {
-			return false;
-		}
+		return processosNaoAlocados.ListaVazia();
 	}
+
+	public void excluirProcesso(Processo proc) throws ProcessoInexistenteException {
+		processosNaoAlocados.removeProcessoLista(proc);
+	}
+
+	public void moverProcesso(Processo proc) throws ProcessoInexistenteException{
+		Processo aux = processosNaoAlocados.getProcessoPorIdentificador(proc.getID());
+		processosAlocados.adicionarProcessolista(aux);
+		processosNaoAlocados.removeProcessoLista(proc);
+	}
+
+	public Processo proximoProcesso(int clock, int indice) throws ProcessoInexistenteException{
+		Processo proc = processosNaoAlocados.getProcessoPorIndice(indice);
+		if(proc.getTempoInicializacao() <= clock) {
+			return proc;
+		}else {
+			return null;
+		}
+
+	}
+	
+ 
 	
 }
