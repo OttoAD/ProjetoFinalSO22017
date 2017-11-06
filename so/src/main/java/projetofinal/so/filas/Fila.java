283131ff394eb4389f2d7b649bfddb5807efac01
OttoAD <otavioad@hotmail.com>
2017-11-05 22:34:57 -0200

    Iniciando a criação da fila

--- so/src/main/java/projetofinal/so/filas/Fila.java
+++ so/src/main/java/projetofinal/so/filas/Fila.java
@@ -1,5 +1,32 @@
 package projetofinal.so.filas;
 
-public class Fila {
+import java.util.ArrayList;
 
+import projetofinal.so.processos.Processo;
+import projetofinal.so.processos.ProcessoInexistenteException;
+
+public class Fila {
+	
+	private ArrayList<Processo> processos;
+	
+	
+	public Fila() {
+		this.processos = new ArrayList<Processo>();
+	}
+	
+	public void inserirProcesso(Processo proc) throws ProcessoInexistenteException {
+		if(proc!=null) {
+			processos.add(proc);
+		}else {
+			throw new ProcessoInexistenteException("Não se pode inserir processos nulos");
+		}
+	}
+	
+	public void removerProcesso(int identificador) {
+		
+	}
+	
+	public void getProcesso(int identificador) {
+		
+	}
 }
